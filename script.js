2ac5fb82d1796863912a897bd97997c87b8c58c7
Lauren Champs <laurenkchamps@gmail.com>
2023-02-20 11:59:19 +1030

    Build game logic
    Add function to determine which player will win based on move played and output a message declaring the winner.

--- script.js
+++ script.js
@@ -5,12 +5,41 @@ function getComputerChoice (arr) {
 
     //get random item
     const item = arr[randomIndex];
+    
+    console.log(item);
 
     return item;
 }
 
-const plays = ['rock', 'paper', 'scissors'];
+const playOptions = ['rock', 'paper', 'scissors'];
 
-getComputerChoice(plays);
+//Get one of the three play options (rock, paper or scissors) at random for computer's play
+const computerSelection = getComputerChoice(playOptions);
 
-console.log(getComputerChoice(plays));
+//Get result of player selection vs computer selection and return an alert with the message of who won.
+function result(playerSelection, computerSelection) {
+
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        alert('You win! Rock beats scissors');
+    } else if (playerSelection === 'rock' && computerSelection ==='paper') {
+        alert('You lose! Paper beats rock.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        alert('You win! Scissors beats paper')
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        alert('You lose! Rock beats scissors')
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        alert('You win! Paper beats rock')
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        alert('You lose! Scissors beats paper')
+    } else if (playerSelection === computerSelection) {
+        alert('It\'s a tie!')
+    } else {
+        alert('Something went wrong')
+    }
+}
+
+//Input player selection
+const playerSelection = playOptions[0];
+
+//Run one round of game
+result(playerSelection, computerSelection);
