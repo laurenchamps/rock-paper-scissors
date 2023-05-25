f54508ec178014a3445e23f145803999e082fed8
Lauren Champs <laurenkchamps@gmail.com>
2023-05-25 16:56:11 +0930

    Update colours and modal border, add comments

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
- //set options for play
-const playOptions = ['Rock', 'Paper', 'Scissors'];
 let playerScore = 0;
 let computerScore = 0;
 
@@ -7,25 +5,29 @@ const rpsBtns = document.querySelectorAll('.rpsBtn');
 let resultDiv = document.querySelector('div.result');
 let playerScoreboard = document.querySelector('.playerScore');
 let computerScoreboard = document.querySelector('.computerScore');
-
+const winModal = document.querySelector('dialog.playerWins');
+const loseModal = document.querySelector('dialog.computerWins');
+const playAgain = document.querySelectorAll('.reload');
 
 //Select a random option for the computer to play
 function getComputerChoice () {
+    const playOptions = ['Rock', 'Paper', 'Scissors'];
     return computerSelection = playOptions[Math.floor(Math.random() * playOptions.length)];
 }
 
+// Play game
 function playGame() {
+    //Set event listeners on all buttons
     rpsBtns.forEach(btn => btn.addEventListener('click', () => {
         const playerSelection = btn.id;
-
         const computerSelection = getComputerChoice();
 
+        // Call playRound function
         playRound(playerSelection, computerSelection);
     }));
 }
 
-//Get result of player selection vs computer selection and return an alert 
-// with the message of who won.
+//Get result of player selection vs computer selection, update and display score.
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection.toLowerCase()) {
@@ -35,7 +37,8 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === 'paper' && computerSelection.toLowerCase() === 'rock') || 
         (playerSelection === 'scissors' && computerSelection.toLowerCase() === 'paper')
     ) {
-        resultDiv.textContent = `You win. ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.toLowerCase()}.`;
+        resultDiv.textContent = `You win. ${playerSelection.charAt(0).toUpperCase() 
+            + playerSelection.slice(1)} beats ${computerSelection.toLowerCase()}.`;
         playerScore += 1;
         playerScoreboard.textContent = `Player: ${playerScore}`;
     } else {
@@ -46,28 +49,28 @@ function playRound(playerSelection, computerSelection) {
     finalResult(playerScore, computerScore);
 }
 
+// When player or computer reaches 5 points, display modal and end game
 function finalResult (playerScore, computerScore) {
-    const winModal = document.querySelector('dialog.playerWins');
-    const loseModal = document.querySelector('dialog.computerWins');
-    const playAgain = document.querySelectorAll('.reload');
-
-    if (playerScore === 5) {
+       if (playerScore === 5) {
         winModal.showModal();
         gameOver();
     } else if (computerScore === 5) {
         loseModal.showModal();
-        loseModal.style.backgroundColor = '#D9230D';
+        loseModal.style.border = '10px dotted #e23838'
         gameOver();
     };
 
+    // Reload page if 'Play again' clicked
     playAgain.forEach(play => play.addEventListener('click', () => {
         window.location.reload();
     }));
 }
 
+// Disable buttons when game ends
 function gameOver () {
     rpsBtns.forEach(btn => btn.disabled = true);
 }
 
+// Call playGame and finalResult functions
 playGame();
 finalResult();
